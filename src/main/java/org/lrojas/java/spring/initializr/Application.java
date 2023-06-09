7f6e44b2d8414e13c39bf98ab8e6453a482eaca6
Legach <lautarito-hernan.14@live.com.ar>
2023-06-09 15:08:40 -0300

    Update

--- src/main/java/org/lrojas/java/spring/initializr/Application.java
+++ src/main/java/org/lrojas/java/spring/initializr/Application.java
@@ -13,7 +13,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+		return String.format("Hola %s!", name);
 	}
 }
